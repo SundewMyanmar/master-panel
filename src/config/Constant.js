c5aefc213ce7a25feac527f72608ca2de97dd344
m0n-hash <saw.yoetha@gmail.com>
2019-06-21 15:33:15 +0630

    Fix label

--- src/config/Constant.js
+++ src/config/Constant.js
@@ -1,4 +1,4 @@
-export const APP_NAME = "MeFood Lunch Box";
+export const APP_NAME = "Web Panel";
 export const APP_VERSION = "0.1.0";
 export const API_URL = "http://localhost:8080/";
 export const DATE_FORMAT = "YYYY-MM-DD";
